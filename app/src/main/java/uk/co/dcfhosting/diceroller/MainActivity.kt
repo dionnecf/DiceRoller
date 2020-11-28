c5690a40c266be9afe6b35e8a04815278fdaee95
Dionne Condor-Farrell <info@dcfhosting.co.uk>
2020-11-28 16:59:37 +0000

    move declaration of diceImage to the start of the class and use lateinit var, initialise diceImage in onCreate, remove declaration of diceImage variable from rollDice function

--- app/src/main/java/uk/co/dcfhosting/diceroller/MainActivity.kt
+++ app/src/main/java/uk/co/dcfhosting/diceroller/MainActivity.kt
@@ -7,6 +7,9 @@ import android.widget.TextView
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -31,7 +36,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
