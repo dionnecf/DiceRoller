c6d7b808ef39b11795a401617235ad3b7f1734ce
Dionne Condor-Farrell <info@dcfhosting.co.uk>
2020-11-28 16:23:20 +0000

    added id to text view, added rollDice function, added random number generator and modified text view text when button clicked

--- app/src/main/java/uk/co/dcfhosting/diceroller/MainActivity.kt
+++ app/src/main/java/uk/co/dcfhosting/diceroller/MainActivity.kt
@@ -2,9 +2,8 @@ package uk.co.dcfhosting.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
-import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +13,13 @@ class MainActivity : AppCompatActivity() {
         rollButton.text = "Let's Roll"
 
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultTV: TextView = findViewById(R.id.result_text)
+        val randomInt = Random().nextInt(6) + 1
+        resultTV.text = randomInt.toString()
     }
 }
